1c9fb802bd590be0284c1a04d58ca5d255e92692
robin-mu <robin-mu@users.noreply.github.com>
2025-09-29 19:55:41 +0200

    Add status bar to bookmark window

--- src/gui/bookmark_window.py
+++ src/gui/bookmark_window.py
@@ -1,4 +1,6 @@
+from PySide6.QtCore import QEvent
 from PySide6.QtWidgets import QDialog, QTreeWidgetItem, QFileDialog, QDialogButtonBox
+from PySide6.QtWidgets import QStatusBar, QWidget
 
 from src.bookmark_library import BookmarkLibrary, BookmarkFolder, Bookmark
 from src.gui.bookmark_gui import Ui_Dialog
@@ -8,10 +10,24 @@ class BookmarkWindow(QDialog, Ui_Dialog):
     def __init__(self, parent=None):
         super(BookmarkWindow, self).__init__(parent)
         self.setupUi(self)
+        self.status_bar = QStatusBar(self)
+        self.gridLayout.addWidget(self.status_bar, self.gridLayout.rowCount(), 0, 1, self.gridLayout.columnCount())
+
+        self.label.installEventFilter(self)
+
         self.bookmark_path_entry.textChanged.connect(self.load_file)
         self.browse_button.pressed.connect(self.browse_file)
         self.bookmark_tree_widget.expanded.connect(self.expanded)
         self.buttonBox.button(QDialogButtonBox.Ok).clicked.connect(self.close)
+
+    def eventFilter(self, obj: QWidget, event):
+        if event.type() == QEvent.Type.Enter:
+            self.status_bar.showMessage(obj.statusTip())
+            return True
+        if event.type() == QEvent.Type.Leave:
+            self.status_bar.clearMessage()
+
+        return False
 
     def browse_file(self):
         filename, ok = QFileDialog.getOpenFileName(self, 'Select a file to load', filter='Firefox bookmark file (places.sqlite)')
